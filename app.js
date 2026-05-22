3c7cd55f039b3ce702ea6869e1c695fca96a6b55
test1 <test1@example.com>
2026-05-22 09:44:36 -0700

    Updated app

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Secure Notes App Running');
+    res.send('Secure Notes App Running version2');
 });
 
 app.listen(3000, () => {
