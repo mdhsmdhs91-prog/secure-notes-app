ed4e47b71dcbe87cbaad56b4689a9fb6336f5921
test <test@test.com>
2026-06-08 06:11:37 -0700

    Added security headers

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 const express = require('express');
+const helmet = require('helmet');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Secure Notes App Running version2');
+app.use(helmet());
+
+app.get('/', (req,res)=>{
+    res.send('Home Page');
 });
 
-app.listen(3000, () => {
-    console.log('Server running on port 3000');
+app.get('/login',(req,res)=>{
+    res.send('<h1>Login Page</h1>');
+});
+
+app.get('/register',(req,res)=>{
+    res.send('<h1>Register Page</h1>');
+});
+
+app.get('/search',(req,res)=>{
+    res.send(req.query.q || 'Search');
+});
+
+app.get('/admin',(req,res)=>{
+    res.send('<h1>Admin Panel</h1>');
+});
+
+app.listen(3000,()=>{
+    console.log('Running');
 });
